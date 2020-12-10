a14eecb8f45acb659a8a27d4ad0d935ab9dbe1d5
Stefan Angelov <rbeecommerce@gmail.com>
2020-12-10 15:07:07 -0800

    Finding the Mode function

---
+++ statistics/mode.py
@@ -0,0 +1,9 @@
+def mode(lst):
+
+    most_occurring = lst[0]
+
+    for item in lst[1:]:
+        if lst.count(item) > lst.count(most_occurring):
+            most_occurring = item
+
+    return most_occurring
